6c91e20aa24b7eac4fbe87943d7d9c73b8da2a5d
MikuniTensai <mikunitensai@gmail.com>
2020-09-26 09:50:24 +0700

    a

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -16,7 +16,7 @@
                       <td>
                           <img src="../hati.png" alt="" style="width: 310px;">
                       </td>
-                      <td>HATI S-1 ITN Malang atau bisa disebut Himpunan Mahasiswa Teknik Informatika S-1 ITN Malang sebuah organisasi yang berdiri pada tanggal 17 September 2008. HATI telah menyelenggarakan beberapa acara seperti Informatica Cup(IFCUP),Seminar PKM,Study Excursie dan Informatika Festival(IFFEST) yang telah hadir tiap tahunnya dan membawa kemeriahan yang berbeda tiap tahun. Karena tiap tahun selalu ada ketua himpunan yang baru.Maka akan ada visi dan misi yang baru dan akan membawa HATI S-1 semakin berkembang.Apa sih visi dan misi HATI S-1 saat ini?.</td>
+                      <td>HATI S-1 ITN Malang atau bisa disebut Himpunan Mahasiswa Teknik Informatika S-1 ITN Malang sebuah organisasi yang berdiri pada tanggal 17 September 2008. HATI telah menyelenggarakan beberapa acara seperti Informatica Cup(IFCUP),Seminar PKM,Study Excursie dan Informatika Festival(IFFEST) yang telah hadir tiap tahunnya dan membawa kemeriahan yang berbeda tiap tahun. Karena tiap tahun selalu ada ketua himpunan yang baru.Maka akan ada visi dan misi yang baru dan akan membawa HATI S-1 semakin berkembang.</td>
                     </tr>
                   </table>
             </div>
